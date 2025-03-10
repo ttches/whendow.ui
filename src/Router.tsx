9683afdf0d4e414309916ef34d584fdd3715ed10
ty <ttches@gmail.com>
2025-03-09 17:13:21 -0700

    removed hash router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,17 +1,17 @@
-import { Route, Routes, HashRouter } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import CreateMeeting from "./Components/CreateMeeting";
 import Meeting from "./Components/Meeting";
 import GetAWord from "./Components/GetAWord";
 
 const AppRouter = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path={`/`} element={<CreateMeeting />} />
         <Route path={`/meeting/:meetingId`} element={<Meeting />} />
         <Route path={`/get-a-word`} element={<GetAWord />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
